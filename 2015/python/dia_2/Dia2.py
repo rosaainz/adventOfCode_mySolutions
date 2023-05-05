0211d4b287d52425517200f52e42fd663b8ee7dc
rosaainz <al2173040136@azc.uam.mx>
2023-05-04 23:25:30 -0600

    Part 2 - Day 2 ... correct :D

--- 2015/python/dia_2/Dia2.py
+++ 2015/python/dia_2/Dia2.py
@@ -1,5 +1,5 @@
-with open('input.txt', 'r') as file:
-	inputFile = file.read()
+inputFile = open('input.txt').read()
+	
 
 total = 0
 perimeter = 0
@@ -9,21 +9,19 @@ total_ribbon = 0
 box = inputFile.split("\n")
 
 for i in box:
-	part = i.split("x")
-	for i in part:
-		boxMap = {'l': part[0], 'w': part[1], "h": part[2]}
+	part = sorted(i.split("x"), key=lambda x: int(x))
+	boxMap = {'l': part[0], 'w': part[1], "h": part[2]}
 	area = (2* (int(boxMap['l']) * int(boxMap['w'])) ) + (2* (int(boxMap['w']) * int(boxMap['h'])) ) + (2* ( int(boxMap['h']) * int(boxMap['l']) ) )
 	small = min(int(boxMap['l']) * int(boxMap['w']), int(boxMap['w']) * int(boxMap['h']), int(boxMap['h']) * int(boxMap['l']) )
 	area += small
 
 ##part 2
-## 1. sacar perimetro: (ejemp: 2x3x4) Perímetro = 2largo + 2ancho =  2*2 + 2*3 = 4 + 6 = 10 pies
+## 1. sacar perimetro: (ejemp: 2x3x4) Perímetro = 2largo + 2ancho =  2*2 + 2*3 = 4 + 6 = 10 pies (se calcula con las dimensiones mas pequeñas)
 ## 2. sacar volumen: Volumen = largo x ancho x alto = 2 x 3 x 4 = 24 pies cúbicos
 ## 3. sumarlos volumen + perimetro
-
-	dimensions = list(part)
-	perimeter = 2*int(dimensions[0]) + 2*int(dimensions[1])
-	volume = int(boxMap['l']) * int(boxMap['w']) * int(boxMap['h'])
+	
+	perimeter = 2*(int(part[0])) + 2*(int(part[1]))
+	volume = int(part[0]) * int(part[1]) * int(part[2])
 	ribbon = perimeter + volume
 	total_ribbon += ribbon
 	total += area
