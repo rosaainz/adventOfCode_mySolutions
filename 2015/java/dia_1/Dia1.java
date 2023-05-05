fe75387b1efb851b00b1db74612b5f5ea9489443
rosaainz <al2173040136@azc.uam.mx>
2023-05-04 23:40:29 -0600

    Add correct version Day1 python

--- 2015/java/dia_1/Dia1.java
+++ 2015/java/dia_1/Dia1.java
@@ -4,22 +4,19 @@ import java.util.Scanner;
 
 public class Dia1 {
 	public static void main (String[] args){
-	
-	String locationFile = "/Users/rosasainz/Documents/prog/rosa/versionado/adventOfCode/2015/java/dia_1/input.txt";
-	File file = new File(locationFile);
+		try{
+			File file = new File("/Users/rosasainz/Documents/prog/rosa/versionado/adventOfCode_mySolutions/2015/java/dia_1/input.txt");
+			Scanner scanner = new Scanner(file);
 
-	try{
-		Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()){
+				String line = scanner.nextLine();
+				System.out.print(line);
+			}
 
-		while (scanner.hasNextLine()) {
-			String linea = scanner.nextLine();
-			System.out.print(linea);
-			
+			scanner.close();
+		} catch(IOException e){
+			System.out.print("No se puede leer el archivo");
 		}
-		scanner.close();
-	} catch(IOException e){
-		System.out.print("No se puede leer el archivo");
-	}
 
 	}
 }
