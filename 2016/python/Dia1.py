87ea2d4f05333de3db3aa8d9b1d556acc1301539
rosaainz <al2173040136@azc.uam.mx>
2023-05-25 15:47:28 -0600

    Test 1 - R2, R3

--- 2016/python/Dia1.py
+++ 2016/python/Dia1.py
@@ -8,9 +8,12 @@ input = "R2, L3"
 def calculate_distance(input):
 	list_input = input.split(", ")
 	
+	acum = 0
+
 	for i in list_input:
-		print(i)
-	return 4
+		print(i[0], i[1])
+		acum += int(i[1])
+	return acum
 	
 
 assert calculate_distance(input) == 5, "no coincide"
