2cc4c375f3285a9ce91b66659189b8f8e38d0444
rosaainz <al2173040136@azc.uam.mx>
2023-06-06 16:33:38 -0600

    Create directory lib and tests

--- 2016/python/Dia1.py
+++ 2016/python/Dia1.py
@@ -1,26 +1,30 @@
-#
-# gire a la izquierda (L) o a la derecha (R) 90 grados
-#
 
-
-input = "R2, L3"
-input2 = "R2, R2, R2"
-
-assert calculate_distance(input) == 5, "no coincide"
-assert calculate_distance(input2) == 2, "no coincide"
 
 
 def calculate_distance(input):
 	list_input = input.split(", ")
 	
 	acum = 0
-
 	for i in list_input:
 		print(i[0], i[1])
 		acum += int(i[1])
+
+		#if( i[0] == 'R'):
+		#	acum += int(i[1])
+		#elif( i[0] == 'L'):
+		#	acum -= int(i[1])
+
+		print(acum)
 	return acum
 
 
+input = "R2, L3"
+input2 = "R2, R2, R2"
+
+position = (0,0)
+
+assert calculate_distance(input) == 5, "no coincide"
+assert calculate_distance(input2) == 2, "no coincide"
 
 
 
