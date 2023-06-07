0a80eea8f3f99cbe79c9ce7c252d0a0fcdc2b430
rosaainz <al2173040136@azc.uam.mx>
2023-06-07 14:19:45 -0600

    Define coordinates

--- 2016/python/lib/Dia1.py
+++ 2016/python/lib/Dia1.py
@@ -1,11 +1,19 @@
 
 
-
-def calculate_distance(path):
-	with open(path) as file:
+def calculate_distance(path_file):
+	with open(path_file) as file:
 		input = file.read().strip()
 
 	list_input = input.split(", ")
+
+	coordinates = [
+			{'L': -1, 'R':1}, #N
+			{'L': -1, 'R':1}, #E
+			{'L': 1, 'R':-1}, #S
+			{'L': 1, 'R':-1}  #W
+	]
+
+
 	
 	acum = 0
 	for i in list_input:
