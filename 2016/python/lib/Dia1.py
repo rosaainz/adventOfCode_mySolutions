42308916466ed4c4102c83defc9f382818f9c9ad
rosaainz <al2173040136@azc.uam.mx>
2023-06-08 02:08:57 -0600

    Dia1-2016

--- 2016/python/lib/Dia1.py
+++ 2016/python/lib/Dia1.py
@@ -7,17 +7,32 @@ def calculate_distance(path_file):
 	list_input = input.split(", ")
 
 	quadrant = [
-			{'L': -1, 'R':1}, #N
-			{'L': -1, 'R':1}, #E
-			{'L': 1, 'R':-1}, #S
-			{'L': 1, 'R':-1}  #W
+			{'L': -1, 'R':1}, #N 0
+			{'L': -1, 'R':1}, #E 1 
+			{'L': 1, 'R':-1}, #S 2 
+			{'L': 1, 'R':-1}  #W 3
 	]
 
 	rotation = {'R':1, 'L':-1}
 
-	direction = 0
+	direction = 0 #norte
 	x,y = 0,0
 	
+	for i in list_input:
+		turn = i[0]
+		distance = int(i[1])
+
+		sign = quadrant[direction][turn]   #+
+		
+		if direction%2==0:
+			x += sign * distance
+		else:
+			y += sign * distance
+
+		direction = (direction + 4 + rotation[turn]) % 4
+
+	distance_final = abs(x) + abs(y)
+
 	return distance_final
 
 
