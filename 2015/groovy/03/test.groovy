cf40f41f4067b4363f3ef324e398b775536f962e
3rickDJ <erickdejesusmolina@gmail.com>
2023-07-09 12:16:11 -0600

    refactor: refactor to OOP

--- 2015/groovy/03/test.groovy
+++ 2015/groovy/03/test.groovy
@@ -1,31 +1,30 @@
-def input = "^>v<".toList()
-// def input = "^v^v^v^v^v".toList()
+class Foo{
+  def delivered_houses(List input) {
 
-def file = new File("input.txt").text
-input = file.toList()
-
-
-def coor = [0,0]
-def result =  input.collect {
-  switch(it) {
-    case ">":
-    coor[0] +=1
-    break
-    case "<":
-    coor[0] -=1
-    break
-    case "^":
-    coor[1] +=1
-    break
-    case "v":
-    coor[1] -=1
-    break
+    def coor = [0,0]
+    def result =  input.collect {
+      switch(it) {
+        case ">":
+        coor[0] +=1
+        break
+        case "<":
+        coor[0] -=1
+        break
+        case "^":
+        coor[1] +=1
+        break
+        case "v":
+        coor[1] -=1
+        break
+      }
+      coor.clone()
+    }
+    // println result
+    result << [0,0]
+    // println result
+    result.unique().size()
   }
-  coor.clone()
 }
-// println result
-result << [0,0]
-// println result
 
-
-println result.unique().size()
+def input = new File("input.txt").text.toList()
+println "The result is: ${new Foo().delivered_houses(input)}"
