d8410e16754c8e557e3e2ce85da2b8b8f03930b1
rosaainz <al2173040136@azc.uam.mx>
2023-10-24 15:44:38 -0600

    post solving aoc

--- 2015/day4_2015.py
+++ 2015/day4_2015.py
@@ -5,7 +5,7 @@ secret = 'bgvyzdsv'
 results = []
 for n in range(10000, 999999):
 	result = hashlib.md5((secret + str(n)).encode())
-	if(result.hexdigest().startswith('00000')):
+	if(result.hexdigest().startswith('000000')):
 		results.append((n, result.hexdigest()))
 
 print(results)
