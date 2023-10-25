6853c4e0adfbc5d0adb2de14a8a297623ee84e04
rosaainz <al2173040136@azc.uam.mx>
2023-10-24 23:26:38 -0600

    day 3 part 3

--- 2015/python/day_3/day3.py
+++ 2015/python/day_3/day3.py
@@ -1,21 +1,31 @@
 
 def deliver(directions):
-    x,y = 0,0  #start
-    visit = {(x,y)} #houses visited
+    santa_x ,santa_y, robo_x, robo_y = 0,0, 0,0  #start
+    visit = {(santa_x,santa_y)} #houses visited
 
-    for i in directions:
-        if i == '^':
-            y += 1
-        elif i == 'v':
-            y -= 1
-        elif i == '>':
-            x += 1
-        elif i == '<':
-            x -= 1
-        visit.add((x,y))
+    for i, direction in enumerate(directions):
+        if i % 2  == 0: # santa turn
+            if direction == '^':
+                santa_y += 1
+            elif direction == 'v':
+                santa_y -= 1
+            elif direction == '>':
+                santa_x += 1
+            elif direction == '<':
+                santa_x -= 1
+            visit.add((santa_x,santa_y))
+        else:  # robo santa turn
+            if direction == '^':
+                robo_y += 1
+            elif direction == 'v':
+                robo_y -= 1
+            elif direction == '>':
+                robo_x += 1
+            elif direction == '<':
+                robo_x -= 1
+            visit.add((robo_x, robo_y))
+
     return len(visit)
-
-
 
 
 with open('input.txt', 'r') as file:
